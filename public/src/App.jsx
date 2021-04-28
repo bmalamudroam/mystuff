a926268b56ce4551d4a1388916f288eb61a7753e
Ben Malamud-Roam <bmalamudroam@gmail.com>
2021-04-28 16:34:47 -0700

    created todos component

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
+import Todo from './components/todos/Todo.jsx';
 
 const App = () => (
-  <div>THIS IS WORKING</div>
+  <div>
+    <div>THIS IS WORKING</div>
+    <Todo />
+  </div>
 );
 
 export default App;
---
+++ public/src/components/todos/Todo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+class Todo extends React.Component {
+  render() {
+    return <div>Todos</div>
+  }
+}
+
+export default Todo;
