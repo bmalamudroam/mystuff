3793d1ef32601d40a55f54f523bf095de170729d
Ben Malamud-Roam <bmalamudroam@gmail.com>
2021-04-30 10:55:41 -0700

    todolist retrieves stasks

--- public/src/components/todos/Todo.jsx
+++ public/src/components/todos/Todo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Item from './ListItem.jsx';
+import axios from 'axios';
 
 const TodoWrapper = styled.div`
   display: flex;
@@ -27,14 +28,36 @@ const List = styled.div`
 `;
 
 class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [],
+    }
+    this.setState = this.setState.bind(this);
+  }
+  componentDidMount() {
+    axios.get('/api/todos')
+      .then(({ data }) => {
+        this.setState({ tasks: data }, () => {
+        });
+      })
+      .catch(err => {
+        console.log(`Error retrieving todos from server: ${err}`);
+      });
+  }
   render() {
+    const { tasks } = this.state;
     return (
       <TodoWrapper>
         <Title>
           Todos
         </Title>
         <List>
-
+          {
+            tasks.map(task => (
+              <Item task={task} />
+            ))
+          }
         </List>
       </TodoWrapper>
     )
