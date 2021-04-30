8d461027e448b8be351cad6a5013258aebb265c1
Ben Malamud-Roam <bmalamudroam@gmail.com>
2021-04-30 14:48:34 -0700

    drag hides original

--- public/src/components/todos/dragTest.jsx
+++ public/src/components/todos/dragTest.jsx
@@ -6,7 +6,8 @@ class DragTest extends React.Component {
     super(props);
     this.state = {
       dragStart: {x: 0, y: 0},
-      positionDelta: {x: 0, y: 0}
+      positionDelta: {x: 0, y: 0},
+      currentPosition: {x: 0, y: 0},
     }
     this.setState = this.setState.bind(this);
   }
@@ -16,20 +17,27 @@ class DragTest extends React.Component {
 
   handleDragStart(e) {
     // event.preventDefault();
-    console.log('started');
+    console.log('start');
     this.setState({dragStart: {x: e.clientX, y: 0}});
+    setTimeout(() => {
+      e.target.style.visibility = "hidden";
+    }, 1);
   }
 
-  handleDrag(e) {
+  handleDragEnd(e) {
     event.preventDefault();
-    const { dragStart } = this.state;
-    this.setState({positionDelta: {x: e.clientX - dragStart.x, y: 0}});
+    const { dragStart, positionDelta } = this.state;
+    console.log("end");
+    setTimeout(() => {
+      e.target.style.visibility = "visible";
+    }, 1);
+    this.setState({currentPosition: {x: e.clientX - dragStart.x, y: 0}});
   }
   render() {
     const { positionDelta } = this.state;
     const { x, y } = positionDelta;
     return (
-      <TestBox x={x} draggable="true" onDrag={this.handleDrag.bind(this)} onDragStart={this.handleDragStart.bind(this)}>
+      <TestBox x={x} draggable="true" onDragStart={this.handleDragStart.bind(this)} onDragEnd={this.handleDragEnd.bind(this)}>
         {x}
       </TestBox>
     )
@@ -41,7 +49,8 @@ const TestBox = styled.div`
   border: 2px solid purple;
   width: 100px;
   height: 100px;
-  transform: translateX(${({ x }) => x}px);
+  /* display: ${({dragging}) => (dragging ? "hidden" : "visible")}; */
+  /* transform: translateX(${({ x }) => x}px); */
 `;
 
 export default DragTest;
