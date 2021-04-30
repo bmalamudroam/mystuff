bba2a823fb0f9b102a7de8d7d5ae090a2f164f86
Ben Malamud-Roam <bmalamudroam@gmail.com>
2021-04-30 13:57:57 -0700

    ondrag and ondragstart working in tandum

--- public/src/components/todos/dragTest.jsx
+++ public/src/components/todos/dragTest.jsx
@@ -5,6 +5,7 @@ class DragTest extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      dragStart: {x: 0, y: 0},
       positionDelta: {x: 0, y: 0}
     }
     this.setState = this.setState.bind(this);
@@ -12,15 +13,24 @@ class DragTest extends React.Component {
   // componentDidMount() {
 
   // }
+
+  handleDragStart(e) {
+    // event.preventDefault();
+    console.log('started');
+    this.setState({dragStart: {x: e.clientX, y: 0}});
+  }
+
   handleDrag(e) {
-    debugger;
+    event.preventDefault();
+    const { dragStart } = this.state;
+    this.setState({positionDelta: {x: e.clientX - dragStart.x, y: 0}});
   }
   render() {
     const { positionDelta } = this.state;
     const { x, y } = positionDelta;
     return (
-      <TestBox x={x}>
-        asdasdsd
+      <TestBox x={x} draggable="true" onDrag={this.handleDrag.bind(this)} onDragStart={this.handleDragStart.bind(this)}>
+        {x}
       </TestBox>
     )
   }
